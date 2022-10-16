c932eafad615492d76f1e9f45f5a450c7c0a9c9a
LeonidZhukovets <leonid.zhukovets@gmail.com>
2022-10-17 00:30:03 +0300

    add Tabs component (need fixes)

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,19 @@ import UserName from "./Components/UserName";
 import styles from "./App.module.css";
 import Title from "./Components/Title";
 import Hamburger from "./Components/Hamburger";
+import Tabs from "./Components/Tabs";
 
 const App = () => {
   const [isOpen, setOpened] = useState(false);
+  const tabsItem = [
+    { title: "All", id: 1 },
+    { title: "My favorites", id: 2 },
+    { title: "Popular", id: 3 },
+  ];
+  const [activeTabId, setActiveTabId] = useState(1);
+  const handleTabClick = (id: number) => {
+    setActiveTabId(id);
+  };
 
   return (
     <div className={styles.container}>
@@ -32,6 +42,14 @@ const App = () => {
       <Title title={"Sign In"} />
 
       <Hamburger isOpened={isOpen} onClick={() => setOpened(!isOpen)} />
+      <Tabs
+        activeTabId={activeTabId}
+        tabs={tabsItem}
+        onClick={handleTabClick}
+      ></Tabs>
+      {activeTabId === 1 && <div>Tab1 works</div>}
+      {activeTabId === 2 && <div>Tab2 works</div>}
+      {activeTabId === 3 && <div>Tab3 works</div>}
     </div>
   );
 };
