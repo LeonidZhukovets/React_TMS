8bf4ee2eab788da0501ee3c84f217001425ff155
LeonidZhukovets <104865810+LeonidZhukovets@users.noreply.github.com>
2022-10-25 14:37:37 +0300

    Hw 2-3 components (#2)
    * add button and Username components
    
    * add Title components
    
    * add Hamburger component
    
    * add Tabs component (need fixes)
    
    * add input, update burger & tabs components
    
    * fixed tabs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import Button, { ButtonTypes } from "./Components/Button";
+import UserName from "./Components/UserName";
+//@ts-ignore
+import styles from "./App.module.css";
+import Title from "./Components/Title";
+import { BurgerClosedIcon, BurgerOpenedIcon } from "./Assets/icons";
+
+import TabsList from "./Components/TabsList";
+import Input from "./Components/Input";
+
+const App = () => {
+  const [isOpened, setOpened] = useState(false);
+
+  const [inputValue, setInputValue] = useState("");
+
+  const onChange = (value: string) => {
+    setInputValue(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Button
+        title={"Primary"}
+        type={ButtonTypes.Primary}
+        onClick={() => alert("Primary")}
+      />
+      <Button
+        title={"Secondary"}
+        type={ButtonTypes.Secondary}
+        onClick={() => alert("Secondary")}
+      />
+      <Button
+        title={"Error"}
+        type={ButtonTypes.Error}
+        onClick={() => alert("Error")}
+      />
+      <UserName username={"Artem_Malkin"} />
+
+      <Title title={"Sign In"} />
+
+      <Button
+        title={isOpened ? <BurgerClosedIcon /> : <BurgerOpenedIcon />}
+        type={ButtonTypes.Primary}
+        className={styles.BurgerButton}
+        onClick={() => setOpened(!isOpened)}
+      />
+      <TabsList />
+      <Input
+        value={inputValue}
+        onChange={onChange}
+        placeholder={"Placeholder"}
+        title={"Title"}
+        //   error={"Error"}
+        //   disabled
+      />
+      <Input
+        value={inputValue}
+        onChange={onChange}
+        placeholder={"Placeholder"}
+        title={"Title"}
+        //   error={"Error"}
+        disabled
+      />
+      <Input
+        value={inputValue}
+        onChange={onChange}
+        placeholder={"Placeholder"}
+        title={"Title"}
+        error={"Error text"}
+        //   disabled
+      />
+      <div>{inputValue}</div>
     </div>
   );
-}
+};
 
 export default App;
